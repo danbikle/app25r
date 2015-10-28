8129b0029468ee8e277deba2376241d8e2eb0da6
ann <ann@ann.edu>
2015-10-28 07:57:55 +0000

    mo

---
+++ db/csv2cars.rb
@@ -0,0 +1,13 @@
+# ~ann/app25r/db/csv2cars.rb
+
+require  'csv'
+csvrows = CSV.read("#{Rails.root}/public/cars.csv")
+csvrows[1,csvrows.size].each{|row| 
+  p row
+  Car.create(:make => row[0], 
+    :mmodel        => row[1], 
+    :yr            => row[2], 
+    :color         => row[3], 
+    :vin           => row[4], 
+    :owner         => row[5])
+}
