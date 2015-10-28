6743e98599f42b169c4823cbabe65859d0baa470
ann <ann@ann.edu>
2015-10-28 06:23:16 +0000

    mo

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,22 @@
 class WelcomeController < ApplicationController
   def index
   end
+
+  def somejson
+    render :json => "['hello json', {'key': 'value', 'k': 'v', 'imakey2': 'Dan'}]"
+  end
+
+  def morejson
+    @myjson = <<-EOF
+      [
+        'hello json', 
+        {
+          'key': 'value', 
+          'k': 'v', 
+          'imakey2': 'Dan'
+        }
+      ]
+    EOF
+    render :json => @myjson
+  end
 end
